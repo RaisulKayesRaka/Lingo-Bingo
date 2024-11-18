9840c85223bd16bd8e87ea795c94ebc31d81756e
Raisul Kayes Raka <119594099+RaisulKayesRaka@users.noreply.github.com>
2024-11-18 21:33:08 +0600

    Add Nunito font, update index.html with font class, and add ErrorPage component to router

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,13 @@ import { createRoot } from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./index.css";
 import App from "./App.jsx";
+import ErrorPage from "./pages/ErrorPage.jsx";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
+    errorElement: <ErrorPage />,
   },
 ]);
 
